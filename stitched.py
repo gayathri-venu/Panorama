c3fdaf04365e3bdd95145e6bda9550f63a1064c8
Gayathri Venu <gayathrivenu2000@gmail.com>
2020-04-19 13:18:48 +0530

    Update stitched.py

--- stitched.py
+++ stitched.py
@@ -1,3 +1,6 @@
+# USAGE
+# python image_stitching.py --images images/scottsdale --output output.png 
+
 # import the necessary packages
 from imutils import paths
 import numpy as np
@@ -31,45 +34,46 @@ stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
 # if the status is '0', then OpenCV successfully performed image
 # stitching
 if status == 0:
-    # create a 10 pixel border surrounding the stitched image
-    stitched = cv2.copyMakeBorder(stitched, 10, 10, 10, 10,
+	
+		# create a 10 pixel border surrounding the stitched image
+		stitched = cv2.copyMakeBorder(stitched, 10, 10, 10, 10,
 			cv2.BORDER_CONSTANT, (0, 0, 0))
 
-    # convert the stitched image to grayscale and threshold it
-    # such that all pixels greater than zero are set to 255
-    # (foreground) while all others remain 0 (background)
-    gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
+		# convert the stitched image to grayscale and threshold it
+		# such that all pixels greater than zero are set to 255
+		# (foreground) while all others remain 0 (background)
+		gray = cv2.cvtColor(stitched, cv2.COLOR_BGR2GRAY)
+		thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
 
-    # find all external contours in the threshold image then find
-    # the *largest* contour which will be the contour/outline of
-    # the stitched image
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		# find all external contours in the threshold image then find
+		# the *largest* contour which will be the contour/outline of
+		# the stitched image
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 			cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    c = max(cnts, key=cv2.contourArea)
+		cnts = imutils.grab_contours(cnts)
+		c = max(cnts, key=cv2.contourArea)
 
-    # allocate memory for the mask which will contain the
-    # rectangular bounding box of the stitched image region
-    mask = np.zeros(thresh.shape, dtype="uint8")
-    (x, y, w, h) = cv2.boundingRect(c)
-    cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
+		# allocate memory for the mask which will contain the
+		# rectangular bounding box of the stitched image region
+		mask = np.zeros(thresh.shape, dtype="uint8")
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
 
-    # create two copies of the mask: one to serve as our actual
-    # minimum rectangular region and another to serve as a counter
-    # for how many pixels need to be removed to form the minimum
-    # rectangular region
-    minRect = mask.copy()
-    sub = mask.copy()
+		# create two copies of the mask: one to serve as our actual
+		# minimum rectangular region and another to serve as a counter
+		# for how many pixels need to be removed to form the minimum
+		# rectangular region
+		minRect = mask.copy()
+		sub = mask.copy()
 
-    # keep looping until there are no non-zero pixels left in the
-    # subtracted image
-    while cv2.countNonZero(sub) > 0:
-               # erode the minimum rectangular mask and then subtract
-               # the thresholded image from the minimum rectangular mask
-               # so we can count if there are any non-zero pixels left
-		minRect = cv2.erode(minRect, None)
-		sub = cv2.subtract(minRect, thresh)
+		# keep looping until there are no non-zero pixels left in the
+		# subtracted image
+		while cv2.countNonZero(sub) > 0:
+			# erode the minimum rectangular mask and then subtract
+			# the thresholded image from the minimum rectangular mask
+			# so we can count if there are any non-zero pixels left
+			minRect = cv2.erode(minRect, None)
+			sub = cv2.subtract(minRect, thresh)
 
 		# find contours in the minimum rectangular mask and then
 		# extract the bounding box (x, y)-coordinates
@@ -82,14 +86,17 @@ if status == 0:
 		# use the bounding box coordinates to extract the our final
 		# stitched image
 		stitched = stitched[y:y + h, x:x + w]
-	
-# write the output stitched image to disk
-cv2.imwrite(args["output"], stitched)
 
-# display the output stitched image to our screen
-cv2.imshow("Stitched", stitched)
-cv2.waitKey(0)
+	        # write the output stitched image to disk
+	        cv2.imwrite(args["output"], stitched)
 
-# USAGE
-# python image_stitching.py --images images/scottsdale --output output.png 
+	        # display the output stitched image to our screen
+	        cv2.imshow("Stitched", stitched)
+	        cv2.waitKey(0)
+
+
+# otherwise the stitching failed, likely due to not enough keypoints
+# being detected
+else:
+                print("[INFO] image stitching failed ({})".format(status))
 
